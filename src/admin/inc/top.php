65ecb2722cd36c7b2bf44934e71291460700d794
Guerin Theo <theo.guerin@etudiant.univ-rennes1.fr>
2023-06-07 11:41:13 +0000

    Suppression Contenus

--- src/admin/inc/top.php
+++ src/admin/inc/top.php
@@ -55,32 +55,7 @@ session_start();
                                 <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                                 Dashboard
                             </a>
-                            
-                            
-                            <div class="sb-sidenav-menu-heading">Contenus</div>
-                            
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-list"></i></div>
-                                Univers
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="#">Tous les Univers</a>
-                                </nav>
-                            </div>
-                            
-                            
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-                                <div class="sb-nav-link-icon"><i class="fas fa-newspaper"></i></div>
-                                Galaxie
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-                               <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="#">Toutes les galaxies</a>
-                                </nav>
-                            </div>
+
                             
                             
                             
